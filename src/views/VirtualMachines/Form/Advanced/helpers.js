17a227e9a6cac70f7027ffefca09ec867ceef0be
heartbit <alidadisaeid@gmail.com>
2025-01-17 13:18:26 -0600

    feat: yaml data synced to froms in new helper function

--- src/views/VirtualMachines/Form/Advanced/helpers.js
+++ src/views/VirtualMachines/Form/Advanced/helpers.js
@@ -201,3 +201,104 @@ export const updateYamlString = ({ data, objectData, useVmTemplate, networks, di
 
     return yamlString;
 };
+
+const getMemoryParts = (memoryString) => {
+    const match = (memoryString || "").match(/(\d+)(\D+)/);
+    if (match) {
+        return { size: match[1], unit: match[2] };
+    }
+    return { size: null, unit: null };
+};
+
+export const syncYamlStringToForms = ({ yamlDataObject, project, data, setDisks, handleChange }) => {
+    const name = yamlDataObject.metadata?.name || data.name;
+    const namespace = yamlDataObject.metadata?.namespace || data.namespace;
+
+    const newData = {
+        name,
+        namespace,
+        cores: yamlDataObject.spec?.template?.spec?.domain?.cpu?.cores || data.cores,
+        sockets: yamlDataObject.spec?.template?.spec?.domain?.cpu?.sockets || data.sockets,
+        threads: yamlDataObject.spec?.template?.spec?.domain?.cpu?.threads || data.threads,
+        advanced: yamlDataObject,
+    };
+
+    const memory = yamlDataObject.spec?.template?.spec?.domain?.resources?.requests?.memory;
+    const memoryParts = getMemoryParts(memory);
+
+    data.memory = memoryParts.size;
+    data.memoryType = memoryParts.unit;
+
+    const userDataAndNetwork = yamlDataObject.spec?.template?.spec?.volumes.at(-1).cloudInitNoCloud;
+    const userData = jsYaml.load(userDataAndNetwork?.userData);
+
+    if (userData) {
+        data.userName = userData.user;
+        data.password = userData.password;
+    }
+    console.log("data is ____", data);
+    handleChange({ ...data, ...newData });
+
+    if (setDisks && yamlDataObject) {
+        const dataVolumeTemplates = yamlDataObject.spec.dataVolumeTemplates;
+
+        if (dataVolumeTemplates.length) {
+            const deviceDisks = yamlDataObject.spec.template.spec.domain.devices.disks;
+            const volumes = yamlDataObject.spec.template.spec.volumes;
+
+            const newOrImageDisks = dataVolumeTemplates.map((template, i) => {
+                const templateName = template.metadata.name;
+                const storage = template.spec.pvc;
+                const storageClass = storage.storageClassName;
+                const storageSize = storage.resources?.requests?.storage;
+                const accessModes = storage.accessModes ? storage.accessModes[0] : "";
+                const source = template.spec.source;
+                const storageParts = getMemoryParts(storageSize);
+                /*
+                 * Based on this documentation about Disks and Volumes
+                 * We should find a device from this path
+                 *  "template" -> "volumes" => "devices.disks"
+                 *
+                 * https://kubevirt.io/user-guide/storage/disks_and_volumes/
+                 */
+                const volume = volumes.find((vol) => vol.dataVolume?.name == templateName);
+                const deviceDisk = deviceDisks.find((disk) => volume?.name === disk.name);
+
+                /**
+                 * We should find the createType based on images' state
+                 * if an image exists with this url and type then this disk is
+                 * using an image
+                 */
+                let image = project.images.find((item) => {
+                    return item.namespace === namespace && source[item?.type] && source[item.type].url === item.url;
+                });
+
+                let sourceType;
+                let url;
+                if (image) {
+                    sourceType = "http";
+                    url = image.url;
+                } else {
+                    sourceType = Object.keys(source)[0];
+                    url = source[sourceType]?.url;
+                }
+
+                return {
+                    createType: image ? "image" : "new",
+                    diskType: "disk",
+                    busType: deviceDisk?.disk?.bus,
+                    memoryType: storageParts.unit,
+                    size: storageParts.size || "",
+                    storageClass: storageClass,
+                    accessMode: accessModes || "",
+                    image: image ? image.name : "",
+                    disk: "",
+                    type: sourceType,
+                    url: url,
+                };
+            });
+
+            setDisks(newOrImageDisks);
+        }
+    }
+};
