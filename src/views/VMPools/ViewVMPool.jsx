1c917209cf78978ba5bbc85a9be3dfb1c84c1f67
heartbit <alidadisaeid@gmail.com>
2025-01-24 18:49:53 -0600

    feat: vm pool page - tabs

--- src/views/VMPools/ViewVMPool.jsx
+++ src/views/VMPools/ViewVMPool.jsx
@@ -2,19 +2,38 @@ import { useLocation } from "react-router-dom";
 import Page from "../../shared/Page";
 import { VirtualMachinePageTitle } from "../../shared/VirtualMachines";
 import { StatusTemplate } from "../../shared/DataTableTemplates";
+import { TabPanel, TabView } from "primereact/tabview";
+import Grid, { Col } from "../../shared/Grid";
+
 
 export const ViewVMPool = () => {
-    const { state } = useLocation();
+  const { state } = useLocation();
 
-    return (
-        <>
-            <Page
-                title={
-                    <VirtualMachinePageTitle name={state.vmPoolName} status={<StatusTemplate status="Running" />} />
-                }
-            >
-
-            </Page>
-        </>
-    )
+  return (
+    <>
+      <Page
+        title={
+          <VirtualMachinePageTitle name={state.vmPoolName} status={<StatusTemplate status="Running" />} />
+        }
+      >
+        <TabView>
+          <TabPanel header="Overview">
+            <Grid>
+              <Col size></Col>
+            </Grid>
+          </TabPanel>
+          <TabPanel header="Yaml Template">
+            <Grid>
+              <Col size></Col>
+            </Grid>
+          </TabPanel>
+          <TabPanel header="Instances">
+            <Grid>
+              <Col size></Col>
+            </Grid>
+          </TabPanel>
+        </TabView>
+      </Page>
+    </>
+  )
 }
