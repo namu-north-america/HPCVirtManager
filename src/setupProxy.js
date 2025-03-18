b575785b58c4c697dc25ee793091d6234f220532
srinivasan_natarajan <im.srini@gmail.com>
2025-03-18 16:12:20 -0400

    wip : using kubernetes/client-node

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,26 +4,29 @@ module.exports = function (app) {
   app.use(
     "/server",
     createProxyMiddleware({
-      target: "http://148.113.178.79:8001",
+      // target: "http://15.235.45.17:8001",
+      // target: "http://148.113.178.79:8001",
       // target: "http://172.22.48.1:8001",
-      //target: "http://127.0.0.1:8001",
+      target: "http://127.0.0.1:8001",
       changeOrigin: true,
       pathRewrite: {
         "^/server": "", // remove /server prefix
       },
-      timeout: 3000,
+      timeout: 15000,
     })
   );
   app.use(
     "/server1",
     createProxyMiddleware({
-      //  target: "http://15.157.153.228:8001",
-      target: "http://148.113.178.79:30555",
+      // target: "http://15.235.45.17:8001",
+      // target: "http://15.157.153.228:8001",
+      // target: "http://148.113.178.79:30555",
+      target: "http://127.0.0.1:8001",
       changeOrigin: true,
       pathRewrite: {
         "^/server1": "", // remove /server prefix
       },
-      timeout: 3000,
+      timeout: 15000,
     })
   );
 };
